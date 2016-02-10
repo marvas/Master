15648e4defef47e65b35d213422a2365209388c8
Marius Vasshus <marvas@lyse.net>
2016-02-10 14:58:53 +0100

    Added check for input being string.

--- ABB Robot API IronPython/backendIronPy/rapid_robtarget.py
+++ ABB Robot API IronPython/backendIronPy/rapid_robtarget.py
@@ -133,76 +133,80 @@ def edit_and_write_rapid_data_property(rapid_data, property, new_value):
             robtarget_robconf = rapid_data.Value.Robconf.ToString()
             robtarget_extax = rapid_data.Value.Extax.ToString()
 
-            new_value = new_value.translate(None, "[]")
-            if property.lower() == 'trans':
-                trans_list = new_value.split(',')
-                if len(trans_list) == 3:
-                    trans = "[[%d,%d,%d],%s,%s,%s]" % \
-                            (float(trans_list[0]), float(trans_list[1]), float(trans_list[2]),
-                             robtarget_rot, robtarget_robconf, robtarget_extax)
-                    robtarget.FillFromString2(trans)
-                    try:
-                        rapid_data.Value = robtarget
-                        msg = 'Trans updated.'
+            if isinstance(new_value, basestring):
+                new_value = new_value.translate(None, "[]")
+                if property.lower() == 'trans':
+                    trans_list = new_value.split(',')
+                    if len(trans_list) == 3:
+                        trans = "[[%d,%d,%d],%s,%s,%s]" % \
+                                (float(trans_list[0]), float(trans_list[1]), float(trans_list[2]),
+                                 robtarget_rot, robtarget_robconf, robtarget_extax)
+                        robtarget.FillFromString2(trans)
+                        try:
+                            rapid_data.Value = robtarget
+                            msg = 'Trans updated.'
+                            return rapid_data, msg
+                        except Exception, err:
+                            return rapid_data, err
+                    else:
+                        msg = 'Incorrect format of x,y,z: ex. \'10,50,0\'.'
                         return rapid_data, msg
-                    except Exception, err:
-                        return rapid_data, err
-                else:
-                    msg = 'Incorrect format of x,y,z: ex. \'10,50,0\'.'
-                    return rapid_data, msg
-            elif property.lower() == 'rot':
-                rot_list = new_value.split(',')
-                if len(rot_list) == 4:
-                    rot = "[%s,[%d,%d,%d,%d],%s,%s]" % \
-                          (robtarget_trans, float(rot_list[0]),float(rot_list[1]),float(rot_list[2]),float(rot_list[3]),
-                           robtarget_robconf, robtarget_extax)
-                    robtarget.FillFromString2(rot)
-                    try:
-                        rapid_data.Value = robtarget
-                        msg = 'Rot updated.'
+                elif property.lower() == 'rot':
+                    rot_list = new_value.split(',')
+                    if len(rot_list) == 4:
+                        rot = "[%s,[%d,%d,%d,%d],%s,%s]" % \
+                              (robtarget_trans, float(rot_list[0]),float(rot_list[1]),float(rot_list[2]),float(rot_list[3]),
+                               robtarget_robconf, robtarget_extax)
+                        robtarget.FillFromString2(rot)
+                        try:
+                            rapid_data.Value = robtarget
+                            msg = 'Rot updated.'
+                            return rapid_data, msg
+                        except Exception, err:
+                            return rapid_data, err
+                    else:
+                        msg = 'Incorrect format of q1,q2,q3,q4: ex. \'0,0,1,0\'.'
                         return rapid_data, msg
-                    except Exception, err:
-                        return rapid_data, err
-                else:
-                    msg = 'Incorrect format of q1,q2,q3,q4: ex. \'0,0,1,0\'.'
-                    return rapid_data, msg
-            elif property.lower() == 'robconf':
-                conf_data_list = new_value.split(',')
-                if len(conf_data_list) == 4:
-                    robconf = "[%s,%s,[%d,%d,%d,%d],%s]" % \
-                              (robtarget_trans, robtarget_rot,
-                               float(conf_data_list[0]),float(conf_data_list[1]),float(conf_data_list[2]),
-                               float(conf_data_list[3]),robtarget_extax)
-                    robtarget.FillFromString2(robconf)
-                    try:
-                        rapid_data.Value = robtarget
-                        msg = 'Robconf updated.'
-                        return  rapid_data, msg
-                    except Exception, err:
-                        return rapid_data, err
-                else:
-                    msg = 'Incorrect format of Cf1,Cf4,Cf6,Cfx: ex. \'1,0,1,0\'.'
-                    return rapid_data, msg
-            elif property.lower() == 'extax':
-                extax_list = new_value.split(',')
-                if len(extax_list) == 6:
-                    extax = "[%s,%s,%s,[%d,%d,%d,%d,%d,%d]]" % \
-                            (robtarget_trans, robtarget_rot, robtarget_robconf,
-                             float(extax_list[0]),float(extax_list[1]),
-                             float(extax_list[2]),float(extax_list[3]),
-                             float(extax_list[4]),float(extax_list[5]))
-                    robtarget.FillFromString2(extax)
-                    try:
-                        rapid_data.Value = robtarget
-                        msg = 'Extax updated.'
+                elif property.lower() == 'robconf':
+                    conf_data_list = new_value.split(',')
+                    if len(conf_data_list) == 4:
+                        robconf = "[%s,%s,[%d,%d,%d,%d],%s]" % \
+                                  (robtarget_trans, robtarget_rot,
+                                   float(conf_data_list[0]),float(conf_data_list[1]),float(conf_data_list[2]),
+                                   float(conf_data_list[3]),robtarget_extax)
+                        robtarget.FillFromString2(robconf)
+                        try:
+                            rapid_data.Value = robtarget
+                            msg = 'Robconf updated.'
+                            return  rapid_data, msg
+                        except Exception, err:
+                            return rapid_data, err
+                    else:
+                        msg = 'Incorrect format of Cf1,Cf4,Cf6,Cfx: ex. \'1,0,1,0\'.'
                         return rapid_data, msg
-                    except Exception, err:
-                        return rapid_data, err
+                elif property.lower() == 'extax':
+                    extax_list = new_value.split(',')
+                    if len(extax_list) == 6:
+                        extax = "[%s,%s,%s,[%d,%d,%d,%d,%d,%d]]" % \
+                                (robtarget_trans, robtarget_rot, robtarget_robconf,
+                                 float(extax_list[0]),float(extax_list[1]),
+                                 float(extax_list[2]),float(extax_list[3]),
+                                 float(extax_list[4]),float(extax_list[5]))
+                        robtarget.FillFromString2(extax)
+                        try:
+                            rapid_data.Value = robtarget
+                            msg = 'Extax updated.'
+                            return rapid_data, msg
+                        except Exception, err:
+                            return rapid_data, err
+                    else:
+                        msg = 'Incorrect format of Eax_a,Eax_b,Eax_c,Eax_d,Eax_e,Eax_f: ex \'9E9,9E9,9E9,9E9,9E9,9E9\'.'
+                        return rapid_data, msg
                 else:
-                    msg = 'Incorrect format of Eax_a,Eax_b,Eax_c,Eax_d,Eax_e,Eax_f: ex \'9E9,9E9,9E9,9E9,9E9,9E9\'.'
+                    msg = 'Property not of type trans, rot, robconf or extax.'
                     return rapid_data, msg
             else:
-                msg = 'Property not of type trans, rot, robconf or extax.'
+                msg = 'Input is not string.'
                 return rapid_data, msg
         except Exception, err:
             return rapid_data, err
@@ -233,31 +237,37 @@ def edit_and_write_rapid_data(rapid_data, trans, rot, robconf, extax):
         try:
             robtarget = rapid_data.Value
 
-            trans = trans.translate(None, "[]")
-            rot = rot.translate(None, "[]")
-            robconf = robconf.translate(None, "[]")
-            extax = extax.translate(None, "[]")
+            #Checks if the input is string
+            if (isinstance(trans, basestring) and isinstance(rot, basestring) and
+                isinstance(robconf, basestring) and isinstance(extax, basestring)):
+                trans = trans.translate(None, "[]")
+                rot = rot.translate(None, "[]")
+                robconf = robconf.translate(None, "[]")
+                extax = extax.translate(None, "[]")
 
-            trans_list = trans.split(',')
-            rot_list = rot.split(',')
-            robconf_list = robconf.split(',')
-            extax_list = extax.split(',')
-            if (len(trans_list) == 3) and (len(rot_list) == 4) and (len(robconf_list) == 4) and (len(extax_list) == 6):
-                new_robtarget = "[[%d,%d,%d],[%d,%d,%d,%d],[%d,%d,%d,%d],[%d,%d,%d,%d,%d,%d]]" % \
-                                (float(trans_list[0]), float(trans_list[1]), float(trans_list[2]),
-                                 float(rot_list[0]), float(rot_list[1]), float(rot_list[2]), float(rot_list[3]),
-                                 float(robconf_list[0]), float(robconf_list[1]), float(robconf_list[2]),
-                                 float(robconf_list[3]), float(extax_list[0]), float(extax_list[1]),
-                                 float(extax_list[2]), float(extax_list[3]), float(extax_list[4]), float(extax_list[5]))
-                robtarget.FillFromString2(new_robtarget)
-                try:
-                    rapid_data.Value = robtarget
-                    msg = 'Robtarget updated.'
+                trans_list = trans.split(',')
+                rot_list = rot.split(',')
+                robconf_list = robconf.split(',')
+                extax_list = extax.split(',')
+                if (len(trans_list) == 3) and (len(rot_list) == 4) and (len(robconf_list) == 4) and (len(extax_list) == 6):
+                    new_robtarget = "[[%d,%d,%d],[%d,%d,%d,%d],[%d,%d,%d,%d],[%d,%d,%d,%d,%d,%d]]" % \
+                                    (float(trans_list[0]), float(trans_list[1]), float(trans_list[2]),
+                                     float(rot_list[0]), float(rot_list[1]), float(rot_list[2]), float(rot_list[3]),
+                                     float(robconf_list[0]), float(robconf_list[1]), float(robconf_list[2]),
+                                     float(robconf_list[3]), float(extax_list[0]), float(extax_list[1]),
+                                     float(extax_list[2]), float(extax_list[3]), float(extax_list[4]), float(extax_list[5]))
+                    robtarget.FillFromString2(new_robtarget)
+                    try:
+                        rapid_data.Value = robtarget
+                        msg = 'Robtarget updated.'
+                        return rapid_data, msg
+                    except Exception, err:
+                        return rapid_data, err
+                else:
+                    msg = 'Incorrect format of input data.'
                     return rapid_data, msg
-                except Exception, err:
-                    return rapid_data, err
             else:
-                msg = 'Incorrect format of input data.'
+                msg = 'Input is not string.'
                 return rapid_data, msg
         except Exception, err:
             return rapid_data, err
