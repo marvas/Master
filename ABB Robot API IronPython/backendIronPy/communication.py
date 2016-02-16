a35bac6d79fe405972f613955fa5c634a97c8359
Marius Vasshus <marvas@lyse.net>
2016-02-16 12:40:49 +0100

    Tiny cleanup

--- ABB Robot API IronPython/backendIronPy/communication.py
+++ ABB Robot API IronPython/backendIronPy/communication.py
@@ -6,8 +6,8 @@ The communication module has basic functions for initial communication with robo
 import clr
 clr.AddReferenceToFileAndPath(
         'C:\\Program Files (x86)\\ABB Industrial IT\\Robotics IT\\SDK\PCSDK 6.02\\ABB.Robotics.Controllers.PC.dll')
-import ABB.Robotics.Controllers as ctrlrs
 # clr.AddReferenceToFileAndPath('ABB.Robotics.Controllers.PC.dll')
+import ABB.Robotics.Controllers as ctrlrs
 
 
 
