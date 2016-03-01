cc0572214920e116028e7e3498c88c1ffa2ae796
Marius Vasshus <marvas@lyse.net>
2016-03-01 11:04:36 +0100

    Functions are working, need to look at returns and comments

--- ABB Robot API REST/backendREST/communication.py
+++ ABB Robot API REST/backendREST/communication.py
@@ -1,37 +1,123 @@
 """
 The communication module has communication with robot through Robot Web Services. Robot Web Services uses REST to
-identify resources and HTTP to communicate. Application data can be sent as XML or JSON back to client
-through WebSockets.
+identify resources and HTTP to communicate.
 """
 
 import requests
 from requests.auth import HTTPDigestAuth
-from ws4py.client.threadedclient import WebSocketClient
+
 
 """
+Connects to a robot with the specified ip address, username and password.
+Local means the VC.
+
+Args:
+    String: IP address
+    String: username
+    String: password
+Returns:
+    Boolean: Indicates if connection to robot was successful.
+    String: Message with the outcome
+Examples:
+    None
 """
-def subscribe(ipaddress,username,password):
-    # if ipaddress == 'default':
-    #     system_url = 'http://{0}/rw/system?json=1'.format('localhost:8080')
-    # else:
-    #     system_url = 'http://{0}/rw/system?json=1'.format(ipaddress)
-    system_url = 'http://{0}/rw/system?json=1'.format(ipaddress)
-    digest_auth = HTTPDigestAuth(username, password)
-    # payload = {'resources':['1','2','3'],
-    #            '1':'/rw/panel/speedratio',
-    #            '1-p':'1',
-    #            '2':'/rw/panel/ctrlstate',
-    #            '2-p':'1',
-    #            '3':'/rw/panel/opmode',
-    #            '3-p':'1'}
-    response = requests.get(system_url, auth=digest_auth)
-    print response.json()
-    if response.status_code == 200:
-        # location = response.headers['Location']
-        cookie = 'ABBCX={0}'.format(response.cookies['ABBCX'])
-        return True
+
+def connect_robot_with_ipaddr(ipaddress, username, password):
+    if isinstance(ipaddress, basestring) and isinstance(username, basestring) and isinstance(password, basestring):
+        # Inserts ip address into the url
+        if ipaddress.lower() == 'local':
+            url = 'http://{0}/rw/system?json=1'.format('localhost:80')
+        else:
+            url = 'http://{0}/rw/system?json=1'.format(ipaddress.lower())
+        digest_auth = HTTPDigestAuth(username, password)
+        try:
+            response = requests.get(url, auth = digest_auth)
+            if response.status_code == 200:
+                # Gets the system information
+                rob_info_dict = response.json()['_embedded']['_state'][0]
+                rob_info = 'System name: %s, System ID: %s, Controller Version: %s' % \
+                           (rob_info_dict['name'], rob_info_dict['sysid'], rob_info_dict['rwversion'])
+                return True, rob_info, digest_auth, response.cookies
+            else:
+                err = 'Something went wrong. Status code: ' + str(response.status_code)
+                return False, err, None, None
+        except Exception:
+            err = 'Can\'t connect to the specified robot. Something may be wrong with input arguments.'
+            return False, err, None, None
     else:
-        print 'Error subscribing: ' + str(response.status_code)
-        return False
+        err = 'Something wrong with arguments. Needs to be string.'
+        return False, err, None, None
 
-print subscribe('152.94.0.38','Default User','robotics')
+
+"""
+Connects to a robot with the specified ip address and default user.
+Local means the VC.
+
+Args:
+    String: IP address
+Returns:
+    Boolean: Indicates if connection to robot was successful.
+    String: Message with the outcome
+Examples:
+    None
+"""
+
+def connect_robot_with_ipaddr_def_user(ipaddress):
+    if isinstance(ipaddress, basestring):
+        # Inserts ip address into the url
+        if ipaddress.lower() == 'local':
+            url = 'http://{0}/rw/system?json=1'.format('localhost:80')
+        else:
+            url = 'http://{0}/rw/system?json=1'.format(ipaddress.lower())
+        digest_auth = HTTPDigestAuth('Default User', 'robotics')
+        try:
+            response = requests.get(url, auth = digest_auth)
+            if response.status_code == 200:
+                # Gets the system information
+                rob_info_dict = response.json()['_embedded']['_state'][0]
+                rob_info = 'System name: %s, System ID: %s, Controller Version: %s' % \
+                           (rob_info_dict['name'], rob_info_dict['sysid'], rob_info_dict['rwversion'])
+                return True, rob_info, digest_auth, response.cookies
+            else:
+                err = 'Something went wrong. Status code: ' + str(response.status_code)
+                return False, err, None, None
+        except Exception, err:
+            # err = 'Can\'t connect to robot with the specified ip address.'
+            return False, err, None, None
+    else:
+        err = 'Something wrong with arguments. Needs to be string.'
+        return False, err, None, None
+
+
+"""
+Log off the robot controller and disposes of the cookies.
+
+Args:
+    String: IP address
+    Requests.cookies.RequestsCookieJar: cookies
+Returns:
+    String: Message with the outcome
+Examples:
+    None
+"""
+
+def logoff_robot_controller(ipaddress, cookies):
+    if isinstance(ipaddress, basestring):
+        if ipaddress.lower() == 'local':
+                url = 'http://{0}/logout'.format('localhost:80')
+        else:
+                url = 'http://{0}/logout'.format(ipaddress.lower())
+        try:
+            response = requests.get(url, cookies = cookies)
+            if response.status_code == 200:
+                return 'Logout successful.'
+            else:
+                return 'Logout failed. ' + str(response.status_code)
+        except Exception, err:
+            return err
+    else:
+        err = 'Something wrong with arguments. Needs to be string.'
+        return err
+
+
+
