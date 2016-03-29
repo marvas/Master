6ae67990a94f598c6bc2dfc2f78552b89e6a4dee
Marius Vasshus <marvas@lyse.net>
2016-03-29 13:08:24 +0200

    Added connection time test to REST

---
+++ ABB Robot API REST/testscripts/test_connection_time.py
@@ -0,0 +1,30 @@
+"""
+Module for testing the time it takes to connect and logon to a controller with REST API.
+The result is written to file.
+"""
+
+
+import time
+
+import frontendREST.com.communication as communication
+
+
+
+# Connects and logs onto the controller a set amount of times and writes the result to a file.
+for i in range(100):
+    # Timestamp start
+    start_time = time.clock()
+    # Connects to the specified controller and logs on with default user.
+    connected, _, _, cookies = communication.connect_robot_with_ipaddr_def_user('local')
+    # Timestamp stop
+    stop_time = time.clock()
+    # Calculate elapsed time
+    elap_time = stop_time - start_time
+    if not connected:
+        print 'Error connecting and logging in'
+        break
+    print communication.logoff_robot_controller('local', cookies)
+    # Writes the time to the specified text file
+    with open('connection_time_rest.txt', 'a+') as f:
+        f.write('%g\n' % elap_time)
+    f.close()
