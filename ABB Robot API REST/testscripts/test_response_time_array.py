8c37bd17726bb724df0cdc575494d44e7474720b
Marius Vasshus <marvas@lyse.net>
2016-04-07 11:41:57 +0200

    Added response_time_array test REST

---
+++ ABB Robot API REST/testscripts/test_response_time_array.py
@@ -0,0 +1,38 @@
+"""
+Tests the response time of frontend REST
+"""
+
+import sys
+import time
+
+import frontendREST.com.communication as communication
+import frontendREST.rapid.rapid_array as rapid_array
+
+
+
+ipaddr = 'local'
+
+# Connects to the controller
+connected, _, digest_auth, cookies = communication.connect_robot_with_ipaddr_def_user(ipaddr)
+if not connected:
+    print 'Not connected to controller'
+    sys.exit()
+for i in range(1000):
+    # Start timestamp
+    start_time = time.clock()
+    # Edits the number on the controller
+    msg, cookies = rapid_array.edit_and_write_rapid_data_num(ipaddr, cookies, digest_auth, 'T_ROB1', 'MainModule', 'arr', [i,i+1,i+2,i,i,i+1])
+    # Stop timestamp
+    stop_time = time.clock()
+    # Gets the value from the controller in order to see if it is updated
+    if msg is not 'Array updated.':
+        print 'Error updating variable'
+        sys.exit()
+    # Calculates the time taken
+    elap_time = stop_time - start_time
+    # Writes the time to the specified text file
+    with open('output/response_time_array_rest.txt', 'a+') as f:
+        f.write('%d %g\n' % (i, elap_time))
+    f.close()
+_, msg = communication.logoff_robot_controller('local', cookies)
+print msg
