df1f0832dfde18f08fd2d8af9d8d4d6ed99d88f6
Marius Vasshus <marvas@lyse.net>
2016-04-11 15:48:49 +0200

    Updated testscript

--- ABB Robot API REST/testscripts/test_con_inactivity.py
+++ ABB Robot API REST/testscripts/test_con_inactivity.py
@@ -11,20 +11,22 @@ import frontendREST.rapid.rapid_num as rapid_num
 import frontendREST.rapid.rapid_datatypes as rapid_datatypes
 
 
+ipaddr = '152.94.0.39'
+
 # Connects to the specified controller and logs on with default user.
-_, msg, digest_auth, cookies = communication.connect_robot_with_ipaddr_def_user('local')
+_, msg, digest_auth, cookies = communication.connect_robot_with_ipaddr_def_user(ipaddr)
 print msg
 # Pauses the execution for 15 minutes and then proceeds.
 time.sleep(15*60)
 # Gets information of specified rapid variable
-_, response_dict, cookies = rapid_datatypes.get_rapid_data('local', cookies, digest_auth, 'T_ROB1', 'MainModule', 'number')
+_, response_dict, cookies = rapid_datatypes.get_rapid_data(ipaddr, cookies, digest_auth, 'T_ROB1', 'MainModule', 'number')
 print rapid_num.get_value_tostring(response_dict)
 # Edits and writes the new value of num to controller
-msg, cookies = rapid_num.edit_and_write_rapid_data('local', cookies, digest_auth, 'T_ROB1', 'MainModule', 'number', 12)
+msg, cookies = rapid_num.edit_and_write_rapid_data(ipaddr, cookies, digest_auth, 'T_ROB1', 'MainModule', 'number', 12)
 print msg
 # Gets information of specified rapid variable
-_, response_dict, cookies = rapid_datatypes.get_rapid_data('local', cookies, digest_auth, 'T_ROB1', 'MainModule', 'number')
+_, response_dict, cookies = rapid_datatypes.get_rapid_data(ipaddr, cookies, digest_auth, 'T_ROB1', 'MainModule', 'number')
 print rapid_num.get_value_tostring(response_dict)
 # Logs of the controller.
-_, msg = communication.logoff_robot_controller('local', cookies)
+_, msg = communication.logoff_robot_controller(ipaddr, cookies)
 print msg
