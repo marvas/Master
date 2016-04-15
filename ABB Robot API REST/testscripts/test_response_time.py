62af66561ed19b3bc073df0d20b829a6ac9bd36c
Marius Vasshus <marvas@lyse.net>
2016-04-15 14:51:04 +0200

    Small change in response_time test REST

--- ABB Robot API REST/testscripts/test_response_time.py
+++ ABB Robot API REST/testscripts/test_response_time.py
@@ -7,11 +7,10 @@ import time
 
 import frontendREST.com.communication as communication
 import frontendREST.rapid.rapid_num as rapid_num
-import frontendREST.rapid.rapid_datatypes as rapid_datatypes
 
 
 
-ipaddr = 'local'
+ipaddr = '152.94.0.39'
 
 # Connects to the controller
 connected, _, digest_auth, cookies = communication.connect_robot_with_ipaddr_def_user(ipaddr)
@@ -22,12 +21,10 @@ for i in range(100):
     # Start timestamp
     start_time = time.clock()
     # Edits the number on the controller
-    _, cookies = rapid_num.edit_and_write_rapid_data(ipaddr, cookies, digest_auth, 'T_ROB1', 'MainModule', 'number', i)
+    msg, cookies = rapid_num.edit_and_write_rapid_data(ipaddr, cookies, digest_auth, 'T_ROB1', 'MainModule', 'number', 10)
     # Stop timestamp
     stop_time = time.clock()
-    # Gets the value from the controller in order to see if it is updated
-    _, response_dict, cookies = rapid_datatypes.get_rapid_data(ipaddr, cookies, digest_auth, 'T_ROB1', 'MainModule', 'number')
-    if i != rapid_num.get_value(response_dict):
+    if msg != 'Value updated.':
         print 'Error updating variable'
         sys.exit()
     # Calculates the time taken
