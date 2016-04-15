d35fab1bafc221280db19495b489fbcd384d4ab3
Marius Vasshus <marvas@lyse.net>
2016-04-15 11:52:49 +0200

    Simplified the test.

--- ABB Robot API REST/testscripts/test_connection_time.py
+++ ABB Robot API REST/testscripts/test_connection_time.py
@@ -9,13 +9,14 @@ import time
 import frontendREST.com.communication as communication
 
 
+ipaddr = '152.94.0.39'
 
 # Connects and logs onto the controller a set amount of times and writes the result to a file.
 for i in range(100):
     # Timestamp start
     start_time = time.clock()
     # Connects to the specified controller and logs on with default user.
-    connected, _, _, cookies = communication.connect_robot_with_ipaddr_def_user('local')
+    connected, _, _, cookies = communication.connect_robot_with_ipaddr_def_user(ipaddr)
     # Timestamp stop
     stop_time = time.clock()
     # Calculate elapsed time
@@ -23,7 +24,7 @@ for i in range(100):
     if not connected:
         print 'Error connecting and logging in'
         break
-    logoff, _ = communication.logoff_robot_controller('local', cookies)
+    logoff, _ = communication.logoff_robot_controller(ipaddr, cookies)
     if not logoff:
         print 'Error logging off'
         break
