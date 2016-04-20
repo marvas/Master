089dcf7db565a9853e9dee1159e7d7b39d42840b
Marius Vasshus <marvas@lyse.net>
2016-04-20 11:27:22 +0200

    cleaned pep8

--- ABB Robot API REST/frontendREST/rapid/rapid_speeddata.py
+++ ABB Robot API REST/frontendREST/rapid/rapid_speeddata.py
@@ -5,6 +5,8 @@ Module for reading and setting speeddata.
 import unicodedata
 
 import requests
+import requests.auth
+import requests.cookies
 
 
 base_speeddata_list = [5, 10, 20, 30, 40, 50, 60, 80, 100, 150, 200,
@@ -23,21 +25,23 @@ Examples:
 
 """
 
+
 def get_speeddata_tostring(response_dict):
     if response_dict['dattyp'] == 'speeddata':
         try:
             # Formatting the speeddata to check if it is valid.
             value = response_dict['value']
             # Converts from unicode to normalized string
-            value = unicodedata.normalize('NFKD', value).encode('ascii','ignore')
+            value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
             value = value.translate(None, "[]")
             value_list = value.split(',')
             # Speeddata should consist of 4 numbers.
             if len(value_list) == 4:
                 try:
                     # Checks if it is base speed.
-                    if int(value_list[0]) in base_speeddata_list and int(value_list[1])==500 and int(value_list[2])==5000\
-                        and int(value_list[3])==1000:
+                    if int(value_list[0]) in base_speeddata_list and \
+                                    int(value_list[1]) == 500 and int(value_list[2]) == 5000 and \
+                                    int(value_list[3]) == 1000:
                         res = 'Base speeddata: v%d (%s)' % (int(value_list[0]), response_dict['value'])
                         return res
                     # If not base speed then custom speed.
@@ -78,20 +82,26 @@ Returns:
     String: result message or error
     Requests.cookies.RequestsCookieJar: cookies
 Examples:
-    message, cookies = edit_and_write_rapid_data_base('local', cookies, digest_auth, 'T_ROB1', 'MainModule', 'speed', 'v100')
-    message, cookies = edit_and_write_rapid_data_base('local', cookies, digest_auth, 'T_ROB1', 'MainModule', 'speed', 'v10')
+    message, cookies = edit_and_write_rapid_data_base('local', cookies, digest_auth, 'T_ROB1',
+                                                        'MainModule', 'speed', 'v100')
+    message, cookies = edit_and_write_rapid_data_base('local', cookies, digest_auth, 'T_ROB1',
+                                                        'MainModule', 'speed', 'v10')
 """
 
+
 def edit_and_write_rapid_data_base(ipaddress, cookies, digest_auth, program, module, variable_name, value):
-    if isinstance(ipaddress, basestring) and isinstance(cookies, requests.cookies.RequestsCookieJar) \
-        and isinstance(program, basestring) and isinstance(module, basestring) \
-        and isinstance(variable_name, basestring) and isinstance(value, basestring)\
-        and isinstance(digest_auth, requests.auth.HTTPDigestAuth):
+    if isinstance(ipaddress, basestring) and isinstance(cookies, requests.cookies.RequestsCookieJar) and \
+            isinstance(program, basestring) and isinstance(module, basestring) and \
+            isinstance(variable_name, basestring) and isinstance(value, basestring) and \
+            isinstance(digest_auth, requests.auth.HTTPDigestAuth):
         # Constructs the urls
         if ipaddress.lower() == 'local':
-            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format('localhost:80', program, module, variable_name)
+            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format('localhost:80', program,
+                                                                                               module, variable_name)
         else:
-            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format(ipaddress.lower(), program, module, variable_name)
+            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format(ipaddress.lower(),
+                                                                                               program, module,
+                                                                                               variable_name)
         try:
             # Strips value for all leading and trailing whitespaces
             value = value.strip()
@@ -157,21 +167,25 @@ Examples:
                                                                                     'speed', 100.5, 500, 5000.37, 1000)
 """
 
+
 def edit_and_write_rapid_data(ipaddress, cookies, digest_auth, program, module, variable_name, vel_tcp,
                               vel_orient, vel_lin_extax, vel_lin_rot_extax):
-    if isinstance(ipaddress, basestring) and isinstance(cookies, requests.cookies.RequestsCookieJar) \
-        and isinstance(program, basestring) and isinstance(module, basestring) \
-        and isinstance(variable_name, basestring) and isinstance(vel_tcp, (int, float)) \
-        and isinstance(vel_orient, (int, float)) and isinstance(vel_lin_extax, (int, float)) \
-        and isinstance(vel_lin_rot_extax, (int, float)) and isinstance(digest_auth, requests.auth.HTTPDigestAuth):
+    if isinstance(ipaddress, basestring) and isinstance(cookies, requests.cookies.RequestsCookieJar) and \
+            isinstance(program, basestring) and isinstance(module, basestring) and \
+            isinstance(variable_name, basestring) and isinstance(vel_tcp, (int, float)) and \
+            isinstance(vel_orient, (int, float)) and isinstance(vel_lin_extax, (int, float)) and \
+            isinstance(vel_lin_rot_extax, (int, float)) and isinstance(digest_auth, requests.auth.HTTPDigestAuth):
         # Constructs the url
         if ipaddress.lower() == 'local':
-            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format('localhost:80', program, module, variable_name)
+            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format('localhost:80', program,
+                                                                                               module, variable_name)
         else:
-            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format(ipaddress.lower(), program, module, variable_name)
+            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format(ipaddress.lower(),
+                                                                                               program, module,
+                                                                                               variable_name)
         try:
-            new_speeddata = '[%G,%G,%G,%G]' % (float(vel_tcp), float(vel_orient),
-                                                   float(vel_lin_extax), float(vel_lin_rot_extax))
+            new_speeddata = '[%G,%G,%G,%G]' % \
+                            (float(vel_tcp), float(vel_orient), float(vel_lin_extax), float(vel_lin_rot_extax))
             payload = {'value': new_speeddata}
             response = requests.post(url, cookies=cookies, data=payload)
             # If response includes a new cookie to use, set the new cookie.
