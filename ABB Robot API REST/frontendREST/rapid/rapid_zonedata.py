238d51c79520bb994877588b054084426afb38e1
Marius Vasshus <marvas@lyse.net>
2016-04-20 11:45:33 +0200

    Cleaned pep8

--- ABB Robot API REST/frontendREST/rapid/rapid_zonedata.py
+++ ABB Robot API REST/frontendREST/rapid/rapid_zonedata.py
@@ -5,10 +5,12 @@ Module for reading and setting zonedata.
 import unicodedata
 
 import requests
+import requests.auth
+import requests.cookies
 
 
-#Base zonedata in format 'fine, pzone_tcp, pzone_ori, pzone_eax, zone_ori, zone_leax, zone_reax'.
-#First three points are for tool center point movement and last three point for tool reorientation.
+# Base zonedata in format 'fine, pzone_tcp, pzone_ori, pzone_eax, zone_ori, zone_leax, zone_reax'.
+# First three points are for tool center point movement and last three point for tool reorientation.
 base_zonedata_dict = {'z0':     'FALSE, 0.3, 0.3, 0.3, 0.03, 0.3, 0.03',
                       'z1':     'FALSE, 1, 1, 1, 0.1, 1, 0.1',
                       'z5':     'FALSE, 5, 8, 8, 0.8, 8, 0.8',
@@ -36,21 +38,22 @@ Examples:
     None
 """
 
+
 def get_zonedata_tostring(response_dict):
     if response_dict['dattyp'] == 'zonedata':
         try:
             # Get zonedata
             value = response_dict['value']
             # Converts from unicode to normalized string
-            value = unicodedata.normalize('NFKD', value).encode('ascii','ignore')
+            value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
             value = value.translate(None, "[]")
-            #Checks if the data is base zonedata values
+            # Checks if the data is base zonedata values
             for zone in base_zonedata_dict:
-                #Takes all uppercase to lowercase, and trims all whitespaces and tabs.
+                # Takes all uppercase to lowercase, and trims all whitespaces and tabs.
                 if value.lower() == base_zonedata_dict[zone].translate(None, " \t").lower():
                     res = 'Base zonedata: %s (%s)' % (zone, response_dict['value'])
                     return res
-            #If base zonedata is not found.
+            # If base zonedata is not found.
             res = 'Zonedata: %s' % response_dict['value']
             return res
         except Exception, err:
@@ -79,20 +82,26 @@ Returns:
     String: result message or error
     Requests.cookies.RequestsCookieJar: cookies
 Examples:
-    message, cookies = edit_and_write_rapid_data_base('local', cookies, digest_auth, 'T_ROB1', 'MainModule', 'zone', 'z0')
-    message, cookies = edit_and_write_rapid_data_base('local', cookies, digest_auth, 'T_ROB1', 'MainModule', 'zone', 'z20')
+    message, cookies = edit_and_write_rapid_data_base('local', cookies, digest_auth, 'T_ROB1',
+                                                        'MainModule', 'zone', 'z0')
+    message, cookies = edit_and_write_rapid_data_base('local', cookies, digest_auth, 'T_ROB1',
+                                                        'MainModule', 'zone', 'z20')
 """
 
+
 def edit_and_write_rapid_data_base(ipaddress, cookies, digest_auth, program, module, variable_name, value):
-    if isinstance(ipaddress, basestring) and isinstance(cookies, requests.cookies.RequestsCookieJar) \
-        and isinstance(program, basestring) and isinstance(module, basestring) \
-        and isinstance(variable_name, basestring) and isinstance(value, basestring) \
-        and isinstance(digest_auth, requests.auth.HTTPDigestAuth):
+    if isinstance(ipaddress, basestring) and isinstance(cookies, requests.cookies.RequestsCookieJar) and \
+            isinstance(program, basestring) and isinstance(module, basestring) and \
+            isinstance(variable_name, basestring) and isinstance(value, basestring) and \
+            isinstance(digest_auth, requests.auth.HTTPDigestAuth):
         # Constructs the urls
         if ipaddress.lower() == 'local':
-            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format('localhost:80', program, module, variable_name)
+            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format('localhost:80', program,
+                                                                                               module, variable_name)
         else:
-            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format(ipaddress.lower(), program, module, variable_name)
+            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format(ipaddress.lower(),
+                                                                                               program, module,
+                                                                                               variable_name)
         try:
             # Strips value for all leading and trailing whitespaces
             value = value.strip()
@@ -159,26 +168,32 @@ Examples:
                                                                             'zone', False, 1, 1, 1, 0.1, 1, 0.1)
 """
 
+
 def edit_and_write_rapid_data(ipaddress, cookies, digest_auth, program, module, variable_name, finep,
                               pzone_tcp, pzone_ori, pzone_eax, zone_ori, zone_leax, zone_reax):
-    if isinstance(ipaddress, basestring) and isinstance(cookies, requests.cookies.RequestsCookieJar) \
-        and isinstance(program, basestring) and isinstance(module, basestring) \
-        and isinstance(variable_name, basestring) and (finep == True or finep == False) \
-        and isinstance(pzone_tcp,(int,float)) and isinstance(pzone_ori, (int,float)) \
-        and isinstance(pzone_eax, (int,float)) and isinstance(zone_ori, (int,float)) \
-        and isinstance(zone_leax, (int,float)) and isinstance(zone_reax, (int,float)) \
-        and isinstance(digest_auth, requests.auth.HTTPDigestAuth):
+    if isinstance(ipaddress, basestring) and isinstance(cookies, requests.cookies.RequestsCookieJar) and \
+            isinstance(program, basestring) and isinstance(module, basestring) and \
+            isinstance(variable_name, basestring) and (finep == True or finep == False) and \
+            isinstance(pzone_tcp, (int, float)) and isinstance(pzone_ori, (int, float)) and \
+            isinstance(pzone_eax, (int, float)) and isinstance(zone_ori, (int, float)) and \
+            isinstance(zone_leax, (int, float)) and isinstance(zone_reax, (int, float)) and \
+            isinstance(digest_auth, requests.auth.HTTPDigestAuth):
         # Constructs the url
         if ipaddress.lower() == 'local':
-            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format('localhost:80', program, module, variable_name)
+            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format('localhost:80', program,
+                                                                                               module, variable_name)
         else:
-            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format(ipaddress.lower(), program, module, variable_name)
+            url = 'http://{0}/rw/rapid/symbol/data/RAPID/{1}/{2}/{3}?json=1&action=set'.format(ipaddress.lower(),
+                                                                                               program, module,
+                                                                                               variable_name)
         try:
-            if finep == 1: finep = True
-            if finep == 0: finep = False
-            new_zonedata = '[%s,%G,%G,%G,%G,%G,%G]' % (finep, float(pzone_tcp), float(pzone_ori),
-                                                           float(pzone_eax), float(zone_ori), float(zone_leax),
-                                                           float(zone_reax))
+            if finep == 1:
+                finep = True
+            if finep == 0:
+                finep = False
+            new_zonedata = '[%s,%G,%G,%G,%G,%G,%G]' % \
+                           (finep, float(pzone_tcp), float(pzone_ori), float(pzone_eax), float(zone_ori),
+                            float(zone_leax), float(zone_reax))
             payload = {'value': new_zonedata}
             response = requests.post(url, cookies=cookies, data=payload)
             # If response includes a new cookie to use, set the new cookie.
