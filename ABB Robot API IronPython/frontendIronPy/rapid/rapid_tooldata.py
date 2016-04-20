27ce9233213eac1a186d53126ae0cd0b2dee6583
Marius Vasshus <marvas@lyse.net>
2016-04-20 10:29:10 +0200

    cleaned pep8

--- ABB Robot API IronPython/frontendIronPy/rapid/rapid_tooldata.py
+++ ABB Robot API IronPython/frontendIronPy/rapid/rapid_tooldata.py
@@ -4,7 +4,6 @@ tooldata, as well as displaying the different properties of the tooldata.
 """
 
 
-
 """
 Gets Robhold from tooldata and returns it as a string.
 
@@ -15,6 +14,7 @@ Returns:
 Examples:
     None
 """
+
 
 def get_robhold_tostring(rapid_data):
     if rapid_data.RapidType == 'tooldata':
@@ -38,6 +38,7 @@ Returns:
 Examples:
     None
 """
+
 
 def get_tframe_tostring(rapid_data):
     if rapid_data.RapidType == 'tooldata':
@@ -63,12 +64,13 @@ Examples:
     None
 """
 
+
 def get_tload_tostring(rapid_data):
     if rapid_data.RapidType == 'tooldata':
         try:
             res = 'Tload: [Mass,Cog,Aom,Ix,Iy,Iz] = [%G,%s,%s,%G,%G,%G]' % \
                   (rapid_data.Value.Tload.Mass, rapid_data.Value.Tload.Cog.ToString(),
-                   rapid_data.Value.Tload.Aom.ToString(),rapid_data.Value.Tload.Ix,
+                   rapid_data.Value.Tload.Aom.ToString(), rapid_data.Value.Tload.Ix,
                    rapid_data.Value.Tload.Iy, rapid_data.Value.Tload.Iz)
             return res
         except Exception, err:
@@ -88,6 +90,7 @@ Returns:
 Examples:
     None
 """
+
 
 def get_tooldata_tostring(rapid_data):
     if rapid_data.RapidType == 'tooldata':
@@ -118,17 +121,20 @@ Examples:
     message = edit_and_write_rapid_data_property(rapid_data,'tload', '[1,[0,0,1],[1,0,0,0],0,0,0]')
 """
 
+
 def edit_and_write_rapid_data_property(rapid_data, property, new_value):
     if rapid_data.RapidType == 'tooldata':
         try:
             tooldata = rapid_data.Value
             tooldata_robhold = rapid_data.Value.Robhold.ToString()
             tooldata_tframe = rapid_data.Value.Tframe.ToString()
-            tooldata_tload =  rapid_data.Value.Tload.ToString()
+            tooldata_tload = rapid_data.Value.Tload.ToString()
             if property.lower() == 'robhold':
                 if new_value == True or new_value == False:
-                    if new_value == 1: new_value = True
-                    if new_value == 0: new_value = False
+                    if new_value == 1:
+                        new_value = True
+                    if new_value == 0:
+                        new_value = False
                     robhold = "[%s,%s,%s]" % (new_value, tooldata_tframe, tooldata_tload)
                     tooldata.FillFromString2(robhold)
                     try:
@@ -141,15 +147,15 @@ def edit_and_write_rapid_data_property(rapid_data, property, new_value):
                     msg = 'Input is not boolean.'
                     return msg
             elif property.lower() == 'tframe':
-                #Checks to see if input is string
+                # Checks to see if input is string
                 if isinstance(new_value, basestring):
                     new_value = new_value.translate(None, "[]")
                     tframe_list = new_value.split(',')
                     if len(tframe_list) == 7:
                         tframe = "[%s,[[%G,%G,%G],[%G,%G,%G,%G]],%s]" % \
                                  (tooldata_robhold, float(tframe_list[0]), float(tframe_list[1]), float(tframe_list[2]),
-                                  float(tframe_list[3]), float(tframe_list[4]), float(tframe_list[5]),float(tframe_list[6]),
-                                  tooldata_tload)
+                                  float(tframe_list[3]), float(tframe_list[4]), float(tframe_list[5]),
+                                  float(tframe_list[6]), tooldata_tload)
                         tooldata.FillFromString2(tframe)
                         try:
                             rapid_data.Value = tooldata
@@ -164,7 +170,7 @@ def edit_and_write_rapid_data_property(rapid_data, property, new_value):
                     msg = 'Input is not string.'
                     return msg
             elif property.lower() == 'tload':
-                #Checks to see if input is string
+                # Checks to see if input is string
                 if isinstance(new_value, basestring):
                     new_value = new_value.translate(None, "[]")
                     tload_list = new_value.split(',')
@@ -212,12 +218,13 @@ Examples:
     message = edit_and_write_rapid_data(rapid_data, True, '[0,0,100],[1,0,0,0]', '[1,[0,0,1],[1,0,0,0],0,0,0]')
 """
 
+
 def edit_and_write_rapid_data(rapid_data, robhold, tframe, tload):
     if rapid_data.RapidType == 'tooldata':
         try:
             tooldata = rapid_data.Value
 
-            #Checks if input is string
+            # Checks if input is string
             if isinstance(tframe, basestring) and isinstance(tload, basestring):
                 tframe = tframe.translate(None, "[]")
                 tload = tload.translate(None, "[]")
@@ -225,14 +232,17 @@ def edit_and_write_rapid_data(rapid_data, robhold, tframe, tload):
                 tframe_list = tframe.split(',')
                 tload_list = tload.split(',')
                 if (robhold == True or robhold == False) and (len(tframe_list) == 7) and (len(tload_list) == 11):
-                    if robhold == 1: robhold = True
-                    if robhold == 0: robhold = False
+                    if robhold == 1:
+                        robhold = True
+                    if robhold == 0:
+                        robhold = False
                     new_tooldata = "[%s,[[%G,%G,%G],[%G,%G,%G,%G]],[%G,[%G,%G,%G],[%G,%G,%G,%G],%G,%G,%G]]" % \
                                    (robhold, float(tframe_list[0]), float(tframe_list[1]), float(tframe_list[2]),
                                     float(tframe_list[3]), float(tframe_list[4]), float(tframe_list[5]),
-                                    float(tframe_list[6]), float(tload_list[0]), float(tload_list[1]), float(tload_list[2]),
-                                    float(tload_list[3]), float(tload_list[4]), float(tload_list[5]), float(tload_list[6]),
-                                    float(tload_list[7]), float(tload_list[8]), float(tload_list[9]), float(tload_list[10]))
+                                    float(tframe_list[6]), float(tload_list[0]), float(tload_list[1]),
+                                    float(tload_list[2]), float(tload_list[3]), float(tload_list[4]),
+                                    float(tload_list[5]), float(tload_list[6]), float(tload_list[7]),
+                                    float(tload_list[8]), float(tload_list[9]), float(tload_list[10]))
                     tooldata.FillFromString2(new_tooldata)
                     try:
                         rapid_data.Value = tooldata
