3efdc67f4d3df8029990bec0fb1a816f167365e8
Marius Vasshus <marvas@lyse.net>
2016-04-20 11:47:07 +0200

    cleaned pep8

--- ABB Robot API REST/testscripts/test_con_inactivity.py
+++ ABB Robot API REST/testscripts/test_con_inactivity.py
@@ -19,13 +19,15 @@ print msg
 # Pauses the execution for 15 minutes and then proceeds.
 time.sleep(60*15)
 # Gets information of specified rapid variable
-_, response_dict, cookies = rapid_datatypes.get_rapid_data(ipaddr, cookies, digest_auth, 'T_ROB1', 'MainModule', 'number')
+_, response_dict, cookies = rapid_datatypes.get_rapid_data(ipaddr, cookies, digest_auth,
+                                                           'T_ROB1', 'MainModule', 'number')
 print rapid_num.get_value_tostring(response_dict)
 # Edits and writes the new value of num to controller
 msg, cookies = rapid_num.edit_and_write_rapid_data(ipaddr, cookies, digest_auth, 'T_ROB1', 'MainModule', 'number', 12)
 print msg
 # Gets information of specified rapid variable
-_, response_dict, cookies = rapid_datatypes.get_rapid_data(ipaddr, cookies, digest_auth, 'T_ROB1', 'MainModule', 'number')
+_, response_dict, cookies = rapid_datatypes.get_rapid_data(ipaddr, cookies, digest_auth,
+                                                           'T_ROB1', 'MainModule', 'number')
 print rapid_num.get_value_tostring(response_dict)
 # Logs of the controller.
 _, msg = communication.logoff_robot_controller(ipaddr, cookies)
