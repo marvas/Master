84fcda34a606877b9cb206ec611cf340e3744a75
Marius Vasshus <marvas@lyse.net>
2016-04-22 17:47:45 +0200

    Added integrationtests in irpy

---
+++ ABB Robot API IronPython/integrationtests/communication_test.py
@@ -0,0 +1,94 @@
+"""
+Integration test to test communication functionality towards the virtual controller.
+"""
+
+import unittest
+import sys
+
+import frontendIronPy.com.communication as com
+
+
+class RapidNumTest(unittest.TestCase):
+
+    controller = None
+    # Change robot name if a different robot name is used.
+    robot_name = 'IRB_140_6kg_0.81m'
+
+    # Preparing test
+    def setUp(self):
+        """ Setting up for test """
+
+    # Ending test
+    def tearDown(self):
+        """ Cleaning after test """
+
+        test_desc = self.shortDescription()
+        if test_desc == 'Tests connect_robot_with_name with correct input data.':
+            _, _ = com.disconnect_robot_controller(self.controller)
+        elif test_desc == 'Tests connect_robot_with_ipaddr with correct input data.':
+            _, _ = com.disconnect_robot_controller(self.controller)
+
+    # Tests connect_robot_with_name with correct input data.
+    def test_connect_robot_with_name_correct(self):
+        """ Tests connect_robot_with_name with correct input data """
+        controllers = com.discover_controllers_on_network()
+        self.controller, _, connected = com.connect_robot_with_name(controllers, self.robot_name)
+        self.assertTrue(connected)
+
+    # Tests connect_robot_with_name with incorrect input data
+    def test_connect_robot_with_name_incorrect(self):
+        """ Tests connect_robot_with_name with incorrect input data """
+        controllers = com.discover_controllers_on_network()
+        # Checks if wrong input for robot_name.
+        self.controller, res, connected = com.connect_robot_with_name(controllers, True)
+        self.assertIsInstance(res, Exception)
+        # Checks if wrong input for the discovered controllers.
+        self.controller, res, connected = com.connect_robot_with_name(10, self.robot_name)
+        self.assertIsInstance(res, Exception)
+
+    # Tests connect_robot_with_ipaddr with correct input data.
+    def test_connect_robot_with_ipaddr_correct(self):
+        """ Tests connect_robot_with_ipaddr with correct input data. """
+        controllers = com.discover_controllers_on_network()
+        self.controller, _, connected = com.connect_robot_with_ipaddr(controllers, '127.0.0.1')
+        self.assertTrue(connected)
+
+    # Tests connect_robot_with_ipaddr with incorrect input data.
+    def test_connect_robot_with_ipaddr_incorrect(self):
+        """ Tests connect_robot_with_ipaddr with incorrect input data. """
+        controllers = com.discover_controllers_on_network()
+        # Checks if wrong input for robot_name.
+        self.controller, res, connected = com.connect_robot_with_ipaddr(controllers, True)
+        self.assertIsInstance(res, Exception)
+        # Checks if wrong input for the discovered controllers.
+        self.controller, res, connected = com.connect_robot_with_ipaddr(10, '127.0.0.1')
+        self.assertIsInstance(res, Exception)
+
+    # Tests disconnect_robot_controller with correct input data.
+    def test_disconnect_robot_controller_correct(self):
+        """ Tests disconnect_robot_controller with correct input data. """
+        controllers = com.discover_controllers_on_network()
+        self.controller, _, connected = com.connect_robot_with_ipaddr(controllers, '127.0.0.1')
+        is_disconnected, _ = com.disconnect_robot_controller(self.controller)
+        self.assertTrue(is_disconnected)
+
+    # Tests disconnect_robot_controller with incorrect input data.
+    def test_disconnect_robot_controller_incorrect(self):
+        """ Tests disconnect_robot_controller with incorrect input data. """
+        is_disconnected, _ = com.disconnect_robot_controller(True)
+        self.assertFalse(is_disconnected)
+
+    # Tests is_connected_to_controller with correct input data.
+    def test_is_connected_to_controller_correct(self):
+        """ Tests is_connected_to_controller with correct input data. """
+        controllers = com.discover_controllers_on_network()
+        self.controller, _, connected = com.connect_robot_with_ipaddr(controllers, '127.0.0.1')
+        res = com.is_connected_to_controller(self.controller)
+        self.assertTrue(res)
+
+    # Tests is_connected_to_controller with incorrect input data.
+    def test_is_connected_to_controller_incorrect(self):
+        """ Tests is_connected_to_controller with incorrect input data. """
+        res = com.is_connected_to_controller(10)
+        self.assertIsInstance(res, Exception)
+
--- ABB Robot API IronPython/integrationtests/rapid_num_test.py
+++ ABB Robot API IronPython/integrationtests/rapid_num_test.py
@@ -1,5 +1,5 @@
 """
-Integration test to test rapid_bool functionality towards the virtual controller.
+Integration test to test rapid_num functionality towards the virtual controller.
 """
 
 import unittest
@@ -9,7 +9,7 @@ import frontendIronPy.com.communication as com
 import frontendIronPy.user.user_authorization as user_auth
 import frontendIronPy.user.user_mastership as user_mastership
 import frontendIronPy.rapid.rapid_datatypes as rapid_datatypes
-import frontendIronPy.rapid.rapid_bool as rapid_bool
+import frontendIronPy.rapid.rapid_num as rapid_num
 
 
 class RapidNumTest(unittest.TestCase):
@@ -47,11 +47,11 @@ class RapidNumTest(unittest.TestCase):
         # Checks for any additional cleaning for some cases.
         test_desc = self.shortDescription()
         if test_desc == 'Tests edit_and_write_rapid_data with correct input data':
-            got_var, var_bool = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'var_boolean')
+            got_var, var_number = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'var_number')
             if not got_var:
                 print 'Could not get variable from controller. Tests will not be run.'
                 sys.exit()
-            _ = rapid_bool.edit_and_write_rapid_data(var_bool, True)
+            _ = rapid_num.edit_and_write_rapid_data(var_number, 0)
             released_master, _ = user_mastership.release_and_dispose_master_access(self.mastership)
             if not released_master:
                 print 'Could\'t release mastership on controller. Tests will not be run.'
@@ -61,66 +61,66 @@ class RapidNumTest(unittest.TestCase):
         _, _ = user_auth.logoff_robot_controller(self.controller)
         _, _ = com.disconnect_robot_controller(self.controller)
 
-    # Tests get_state_tostring with correct rapid data
-    def test_get_state_tostring_correct(self):
-        """ Tests get_state_tostring with correct rapid data """
-        got_var, const_bool = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'const_boolean')
-        if not got_var:
-            print 'Could not get variable from controller. Test will not be run.'
-            sys.exit()
-        self.assertEqual(rapid_bool.get_state_tostring(const_bool), 'State = True')
-
-    # Tests get_state_tostring with incorrect rapid data
-    def test_get_state_tostring_incorrect(self):
-        """ Tests get_state_tostring with incorrect rapid data """
+    # Tests get_value_tostring with correct rapid data
+    def test_get_value_tostring_correct(self):
+        """ Tests get_value_tostring with correct rapid data """
         got_var, const_number = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'const_number')
         if not got_var:
             print 'Could not get variable from controller. Test will not be run.'
             sys.exit()
-        self.assertEqual(rapid_bool.get_state_tostring(const_number), 'DataType is num and not bool.')
+        self.assertEqual(rapid_num.get_value_tostring(const_number), 'Value = 1000')
 
-    # Tests get_state with correct rapid data
-    def test_get_state_correct(self):
-        """ Tests get_state with correct rapid data """
+    # Tests get_value_tostring with incorrect rapid data
+    def test_get_value_tostring_incorrect(self):
+        """ Tests get_value_tostring with incorrect rapid data """
         got_var, const_bool = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'const_boolean')
         if not got_var:
             print 'Could not get variable from controller. Test will not be run.'
             sys.exit()
-        self.assertEqual(rapid_bool.get_state(const_bool), True)
+        self.assertEqual(rapid_num.get_value_tostring(const_bool), 'DataType is bool and not num.')
 
-    # Tests get_state with incorrect rapid data
-    def test_get_state_incorrect(self):
-        """ Tests get_state with incorrect rapid data """
+    # Tests get_value with correct rapid data
+    def test_get_value_correct(self):
+        """ Tests get_value with correct rapid data """
         got_var, const_number = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'const_number')
         if not got_var:
             print 'Could not get variable from controller. Test will not be run.'
             sys.exit()
-        self.assertEqual(rapid_bool.get_state(const_number), 'DataType is num and not bool.')
+        self.assertEqual(rapid_num.get_value(const_number), 1000)
+
+    # Tests get_value with incorrect rapid data
+    def test_get_value_incorrect(self):
+        """ Tests get_value with incorrect rapid data """
+        got_var, const_bool = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'const_boolean')
+        if not got_var:
+            print 'Could not get variable from controller. Test will not be run.'
+            sys.exit()
+        self.assertEqual(rapid_num.get_value(const_bool), 'DataType is bool and not num.')
 
     # Tests edit_and_write_rapid_data with correct input data
     def test_edit_and_write_rapid_data_correct(self):
         """ Tests edit_and_write_rapid_data with correct input data """
-        got_var, var_bool = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'var_boolean')
+        got_var, var_number = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'var_number')
         if not got_var:
             print 'Could not get variable from controller. Test will not be run.'
             sys.exit()
-        _ = rapid_bool.edit_and_write_rapid_data(var_bool, False)
-        self.assertEqual(var_bool.Value.ToString().lower(), 'false')
+        _ = rapid_num.edit_and_write_rapid_data(var_number, 10)
+        self.assertEqual(float(var_number.Value), 10)
 
     # Tests edit_and_write_rapid_data with incorrect input data
     def test_edit_and_write_rapid_data_incorrect(self):
         """ Tests edit_and_write_rapid_data with incorrect input data """
         # Tests with wrong rapid data input.
+        got_var, var_boolean = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'var_boolean')
+        if not got_var:
+            print 'Could not get variable from controller. Test will not be run.'
+            sys.exit()
+        res = rapid_num.edit_and_write_rapid_data(var_boolean, 10)
+        self.assertEqual(res, 'DataType is bool and not num.')
+        # Tests with right rapid data input but wrong value as input.
         got_var, var_number = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'var_number')
         if not got_var:
             print 'Could not get variable from controller. Test will not be run.'
             sys.exit()
-        res = rapid_bool.edit_and_write_rapid_data(var_number, False)
-        self.assertEqual(res, 'DataType is num and not bool.')
-        # Tests with right rapid data input but wrong value as input.
-        got_var, var_bool = rapid_datatypes.get_rapid_data(self.controller, 'T_ROB1', 'MainModule', 'var_boolean')
-        if not got_var:
-            print 'Could not get variable from controller. Test will not be run.'
-            sys.exit()
-        res = rapid_bool.edit_and_write_rapid_data(var_bool, 10)
+        res = rapid_num.edit_and_write_rapid_data(var_number, True)
         self.assertIsInstance(res, Exception)
