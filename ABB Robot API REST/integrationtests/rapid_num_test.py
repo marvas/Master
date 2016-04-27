f14d17988ece6061ed2476b6348ec096fc2ec82c
Marius Vasshus <marvas@lyse.net>
2016-04-27 14:45:44 +0200

    Small change rapid_num_test rest

--- ABB Robot API REST/integrationtests/rapid_num_test.py
+++ ABB Robot API REST/integrationtests/rapid_num_test.py
@@ -32,7 +32,7 @@ class RapidNumTest(unittest.TestCase):
         # Checks for any additional cleaning for some cases.
         test_desc = self.shortDescription()
         if test_desc == 'Tests edit_and_write_rapid_data with correct input data':
-            _, _ = rapid_num.edit_and_write_rapid_data('local', self.cookies, self.digest_auth, 'T_ROB1',
+            _, self.cookies = rapid_num.edit_and_write_rapid_data('local', self.cookies, self.digest_auth, 'T_ROB1',
                                                        'MainModule', 'var_number', 0)
 
         # Cleaning for all the test cases
